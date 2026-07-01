683bee1a9db6bcb5a23ddd2a5f13574285ebdd14
Himanshu Yadav <136960172+Himanshu-Yadav-0@users.noreply.github.com>
2026-07-01 23:51:37 +0530

    Time: 0 ms (100%), Space: 2.1 MB (97.11%) - LeetHub

--- 0027-remove-element/0027-remove-element.rs
+++ 0027-remove-element/0027-remove-element.rs
@@ -1,6 +1,17 @@
 impl Solution {
+    // pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+    //     nums.retain(|value| *value != val);
+    //     nums.len() as i32
+    // }
+
     pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-        nums.retain(|value| *value != val);
-        nums.len() as i32
+        let mut k = 0;
+        for i in 0..nums.len(){
+            if nums[i] != val{
+                nums[k] = nums[i];
+                k+=1;
+            }
+        }
+        k as i32
     }
 }
