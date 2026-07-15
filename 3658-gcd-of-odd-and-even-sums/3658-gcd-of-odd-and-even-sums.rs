c11475760abfeae7b0101bb07659c047726b78f3
Himanshu Yadav <136960172+Himanshu-Yadav-0@users.noreply.github.com>
2026-07-15 12:07:01 +0530

    Time: 0 ms (100%), Space: 2 MB (82.76%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn gcd_of_odd_even_sums(n: i32) -> i32 {
+    
+        let sum_odd = (n*((2*1)+((n-1)*2)))/2;
+        let sum_even = (n*((2*2)+((n-1)*2)))/2;
+        Self::find_gcd(&sum_odd, &sum_even)
+    }
+    
+    pub fn find_gcd(a: &i32, b: &i32) -> i32{
+        let reminder = a % b;
+        if reminder == 0{
+            return *b;
+        }
+        
+        Self::find_gcd(&b, &reminder)
+    }
+}
