7424d86d36d1cada2fc256f572a15a428e6dca53
Himanshu Yadav <136960172+Himanshu-Yadav-0@users.noreply.github.com>
2026-07-21 23:20:45 +0530

    Time: 2 ms (3.85%), Space: 2.1 MB (56.15%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.rs
+++ 0075-sort-colors/0075-sort-colors.rs
@@ -1,5 +1,25 @@
 impl Solution {
     pub fn sort_colors(nums: &mut Vec<i32>) {
-        nums.sort();
+        let mut low = 0 as usize;
+        let mut mid = 0 as usize;
+        let mut high = nums.len();
+
+        if high == 0{
+            return ();
+        }
+        
+        while mid < high {
+            println!("{} {} {}", low, mid, high);
+            if nums[mid] == 0 {
+                nums.swap(mid, low);
+                low += 1;
+                mid += 1;
+            } else if nums[mid] == 1 {
+                mid += 1;
+            } else {
+                high -= 1;
+                nums.swap(mid, high);
+            }
+        }
     }
 }
