ca06a592b0183da8e9fb811eeffe7c6becdec3ec
Himanshu Yadav <136960172+Himanshu-Yadav-0@users.noreply.github.com>
2026-07-23 08:58:57 +0530

    Time: 0 ms (100%), Space: 3.7 MB (66.67%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn unique_xor_triplets(nums: Vec<i32>) -> i32 {
+        let n = nums.len() as i32;
+
+        if n <= 2{
+            return n;
+        }
+        let bits = i32::BITS - n.leading_zeros();
+        1 << bits
+    }
+}
